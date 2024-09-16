53ca20296d3b9cba87daa6b8476f4763706b6309
yemaomao <113106177+yemaomao522@users.noreply.github.com>
2024-09-16 23:47:32 +0800

    Update create_bucket.py

--- get_image_url/create_bucket.py
+++ get_image_url/create_bucket.py
@@ -1,1 +1,13 @@
+import vlm
+import oss2
+# -*- coding: utf-8 -*-
+from oss2.credentials import EnvironmentVariableCredentialsProvider
 
+# 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
+auth = oss2.ProviderAuth(EnvironmentVariableCredentialsProvider())
+# 填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
+# yourBucketName填写存储空间名称。oss-cn-beijing-internal.aliyuncs.com
+bucket = oss2.Bucket(auth, 'https://oss-cn-hangzhou.aliyuncs.com', 'lampbucket')
+
+# 设置存储空间为私有读写权限。
+bucket.create_bucket(oss2.models.BUCKET_ACL_PRIVATE)  
