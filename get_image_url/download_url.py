44004335e7e4454ed285ac1ded4310360fa5b881
0Nanayashiki0 <91510824+0Nanayashiki0@users.noreply.github.com>
2024-09-17 10:33:51 +0800

    Update download_url.py

--- get_image_url/download_url.py
+++ get_image_url/download_url.py
@@ -3,8 +3,8 @@ import oss2
 from oss2.credentials import EnvironmentVariableCredentialsProvider
 import requests
 
-# 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
-auth = oss2.ProviderAuth(EnvironmentVariableCredentialsProvider())
+# 从config.py中获取访问凭证
+auth = oss2.Auth(config.OSS_ACCESS_KEY_ID, config.OSS_ACCESS_KEY_SECRET)
 
 # yourEndpoint填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
 # 填写Bucket名称，例如examplebucket。
