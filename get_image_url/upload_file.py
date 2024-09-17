1419561924419f5d58c1df517b29538c34de4ee9
0Nanayashiki0 <91510824+0Nanayashiki0@users.noreply.github.com>
2024-09-17 10:36:58 +0800

    Update upload_file.py

--- get_image_url/upload_file.py
+++ get_image_url/upload_file.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 import oss2
-from oss2.credentials import EnvironmentVariableCredentialsProvider
-# 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
-auth = oss2.ProviderAuth(EnvironmentVariableCredentialsProvider())
+import config
+
+# 从config.py中获取访问凭证
+auth = oss2.Auth(config.OSS_ACCESS_KEY_ID, config.OSS_ACCESS_KEY_SECRET)
+
 # 填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
 # yourBucketName填写存储空间名称。
 bucket = oss2.Bucket(auth, 'https://oss-cn-hangzhou.aliyuncs.com', 'lampbucket')
