c599cdf29a80d1f8824f135e5537a1b42a431c3c
JingXiong <133872613+Dora-Xiong@users.noreply.github.com>
2024-12-17 22:38:41 +0800

    Add files via upload

--- vlm.py
+++ vlm.py
@@ -1,9 +1,8 @@
 from openai import OpenAI
 import os
-import requests
 import json
 
-os.environ["DASHSCOPE_API_KEY"] = ""
+os.environ["DASHSCOPE_API_KEY"] = "sk-fa7af4f6e4ca4060bf4cd1e5d2915aa5"
 def get_response(api_key: str, image_url: str, task: str):
     """
     image_url: 传入图片链接
@@ -14,14 +13,14 @@ def get_response(api_key: str, image_url: str, task: str):
         base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
     )
     if task == "word_recognition":
-        sys_prompt = "你是一个文字识别助手，能够提取出图片里书本上的文字。"
+        sys_prompt = "你是一个文字识别助手，能够提取出图片里的文字。"
         user_prompt = "请提取出图片中的文字内容并输出。"
     elif task == "image_description":
-        sys_prompt = "请你帮小朋友描述图画的内容，请尽量如实描述。"
-        user_prompt = "请描述图片中的画面内容。"
+        sys_prompt = "你是帮助小朋友理解绘本内容的智能助手。"
+        user_prompt = "请你向小朋友描述图片中的画面内容，尽量如实描述。"
     
     completion = client.chat.completions.create(
-        model="qwen-vl-plus",
+        model="qwen-vl-max",
         messages=[
             {
                 "role": "system",
@@ -50,5 +49,5 @@ def get_response(api_key: str, image_url: str, task: str):
     return response["choices"][0]["message"]["content"]
 
 if __name__=='__main__':
-    image_link = "https://s2.loli.net/2024/08/19/L5FJ1DzkGI6NPQl.jpg"
-    print(get_response(os.getenv("DASHSCOPE_API_KEY"), image_link, "image_description"))
+    image_link = "https://i.ibb.co/J5YVB3s/2024-12-17-22-26-15.png"
+    print(get_response(os.getenv("DASHSCOPE_API_KEY"), image_link, "word_recognition"))
